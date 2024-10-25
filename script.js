819cb5f09c9bea10055f0b2aeafe6f3407c9511d
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2024-10-25 17:48:00 +0500

    Результат знакомства с js

---
+++ script.js
@@ -0,0 +1,11 @@
+let time1 = 6
+let time2 = 3
+let time3 = 11
+
+let minutes = time1+time2+time3
+console.log(minutes)
+
+minutes = time2+time3
+console.log('До конца пути осталось', minutes, 'минут')
+
+console.log('Конец пути')
